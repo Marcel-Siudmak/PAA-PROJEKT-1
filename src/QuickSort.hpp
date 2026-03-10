7e23814606c401f85c03642f9c6c25d10609d55d
Marcel-Siudmak <siudmakmarcel7@gmail.com>
2026-03-10 22:46:28 +0100

    Modify partitioning from simple last element to Algorithm Median-Of-Three in QuickSort.hpp file

--- src/QuickSort.hpp
+++ src/QuickSort.hpp
@@ -23,6 +23,32 @@ private:
   }
 
   static int partition(T *table, int left, int right) {
+    // --- Algorytm Median-Of-Three (Mediana z trzech) ---
+    // Wybieramy trzy elementy (pierwszy, środek i ostatni) i ustawiamy je tak,
+    // by na miejsce ostatniego wędrowała Mediana z najgorszych prób, a na
+    // miejsce pierwsze wartość najmniejsza. Zabezpiecza nas to przed tablicami
+    // z góry ułożonymi!
+    int mid = left + (right - left) / 2;
+
+    if (table[mid] < table[left]) {
+      T tmp = table[left];
+      table[left] = table[mid];
+      table[mid] = tmp;
+    }
+    if (table[right] < table[left]) {
+      T tmp = table[left];
+      table[left] = table[right];
+      table[right] = tmp;
+    }
+    // Finalnie z pozostałych "dwóch" wyrzucamy ten mniejszy (medianę) do
+    // table[right] jako Pivot
+    if (table[mid] < table[right]) {
+      T tmp = table[mid];
+      table[mid] = table[right];
+      table[right] = tmp;
+    }
+    // Pivot jest teraz na ostatniej pozycji (table[right]), gotowy na stary
+    // podział Lomuto
     T pivot = table[right];
     int i = left - 1;
 
